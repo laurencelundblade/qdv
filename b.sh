446a5c8ed39767a06a535a87b6b2ad41c7691a85
Laurence Lundblade <lgl@securitytheory.com>
2020-10-09 01:01:32 -0700

    build script improvements

--- b.sh
+++ b.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-function foo {
+function stringpermutations {
     local prefix=$1 # the prefix is the first argument
     local theset=$2 # the set left to process is the second argument
   
@@ -25,7 +25,7 @@ function foo {
 
         if [[ ! -z "$theset" ]]; then
            # The set is not empty, recurse to process it
-           foo "$newprefix" "$theset"
+           stringpermutations "$newprefix" "$theset"
         else
            # The set is empty, just output the new prefix
            echo "$newprefix"
@@ -36,30 +36,30 @@ function foo {
     done
 }
 
-make clean
-make -f qdv/Makefile.min decode_min encode_min
+make clean > /dev/null
+make -f qdv/Makefile.min decode_min encode_min > /dev/null
 qdv/sizes.sh decode_min 
 qdv/sizes.sh encode_min
 
-make clean
-make -f qdv/Makefile.max decode_max encode_max
+make clean > /dev/null
+make -f qdv/Makefile.max decode_max encode_max > /dev/null
 qdv/sizes.sh decode_max 
 qdv/sizes.sh encode_max
 
+# Compute the combinations of ifdefs and write to lines in a file
 set="-DQCBOR_DISABLE_FLOAT_HW_USE -DQCBOR_DISABLE_PREFERRED_FLOAT -DQCBOR_CONFIG_DISABLE_EXP_AND_MANTISSA"
-foo "" "$set" > /tmp/b.$$
+stringpermutations "" "$set" > /tmp/b.$$
 
+# make and test each combination of ifdefs
 while read opts; do
    echo "$opts"
    make clean > /dev/null
-   make "CMD_LINE=$opts" > /dev/null 2>&1
+   # Throw away stdout, but not stderr because that's were compiler warnings and erros show
+   # All the extra error check options here so they are run before releases
+   make "CMD_LINE=$opts -Wall -pedantic-errors -Wextra -Wshadow -Wparentheses -Wconversion -xc -std=c99" 2>&1 >/dev/null | grep -v 'ar: creating'
    ./qcbortest > /tmp/bb.$$
    grep SUMMARY /tmp/bb.$$
-   #if [ $? -eq 0 ]; then
-   #   echo "FAIL FAIL FAIL $opts"
-   #fi
 done < /tmp/b.$$
 
-
-rm /tmp/b.$$
+rm -f /tmp/b.$$ /tmp/bb.$$
 
