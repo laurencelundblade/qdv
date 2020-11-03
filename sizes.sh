3f0b56bb8d22b3b4dd40f19cee48425d9152f82e
Laurence Lundblade <lgl@securitytheory.com>
2020-11-03 03:17:09 -0800

    fix size calculation

--- sizes.sh
+++ sizes.sh
@@ -9,7 +9,7 @@ fun=`nm libqcbor.a | awk '/ [TtSs] /{printf "%s|", $3 }'; echo xxxxxxxxxxxxxx`
 # with the offsets of program text, so they are omitted
 
 nm -n -t d -U $1 |\
-grep ' [Tt] ' |\
+grep ' [TtSs] ' |\
 awk 'NR!=1{printf "%-40s %4s\n", name, $1 - offset }
      {offset=$1; name=$3}' |\
 egrep $fun | sort -r -n  -k 2 | tee /tmp/sizes.$$
