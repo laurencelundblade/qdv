536f0834fc2f84c17ea0b63513e596cfca1ff9f7
Laurence Lundblade <lgl@securitytheory.com>
2021-03-06 08:41:22 -0800

    Increase compiler warning flags, use llvm and gcc, fix so warnings are output correctly

--- b.sh
+++ b.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
+#
+# b.sh -- Before-release comprehensive test and build script for QCBOR
+#
+# Copyright (c) 2019-2021, Laurence Lundblade. All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+#
+# See BSD-3-Clause license in README.md
+#
 
-
+# This creates strings with all the permutations of the compiler ifdefs
 function stringpermutations {
     local prefix=$1 # the prefix is the first argument
     local theset=$2 # the set left to process is the second argument
@@ -36,28 +45,60 @@ function stringpermutations {
     done
 }
 
+
+# Build and print out the code size for smallest configuration 
 make clean > /dev/null
 make -f qdv/Makefile.min decode_min encode_min > /dev/null
 qdv/sizes.sh decode_min 
 qdv/sizes.sh encode_min
 
+# Build and print out the code size for largest configuration
 make clean > /dev/null
 make -f qdv/Makefile.max decode_max encode_max > /dev/null
 qdv/sizes.sh decode_max 
 qdv/sizes.sh encode_max
 
+
 # Compute the combinations of ifdefs and write to lines in a file
-set="-DQCBOR_DISABLE_FLOAT_HW_USE -DQCBOR_DISABLE_PREFERRED_FLOAT -DQCBOR_CONFIG_DISABLE_EXP_AND_MANTISSA -DQCBOR_DISABLE_ENCODE_USAGE_GUARDS -DQCBOR_DISABLE_INDEFINITE_LENGTH_STRINGS -DQCBOR_DISABLE_INDEFINITE_LENGTH_ARRAYS -DQCBOR_DISABLE_UNCOMMON_TAGS"
+set="-DQCBOR_DISABLE_FLOAT_HW_USE"
+set+=" -DQCBOR_DISABLE_PREFERRED_FLOAT"
+set+=" -DQCBOR_CONFIG_DISABLE_EXP_AND_MANTISSA"
+set+=" -DQCBOR_DISABLE_ENCODE_USAGE_GUARDS"
+set+=" -DQCBOR_DISABLE_INDEFINITE_LENGTH_STRINGS"
+set+=" -DQCBOR_DISABLE_INDEFINITE_LENGTH_ARRAYS"
+set+=" -DQCBOR_DISABLE_UNCOMMON_TAGS"
 
 stringpermutations "" "$set" > /tmp/b.$$
 
-# make and test each combination of ifdefs
+
+# The QCBOR makefile uses a minimum of compiler flags so that it will
+# work out-of-the-box with a wide variety of compilers. This is where
+# compilation with a full set of warning flags is performed. This set
+# of flags has been thought through for gcc and clang/llvm.
+warn_flags="-Wall"
+warn_flags+=" -Wextra"
+warn_flags+=" -Wpedantic"
+warn_flags+=" -Wstrict-prototypes"
+warn_flags+=" -Wshadow"
+warn_flags+=" -Wconversion"
+warn_flags+=" -Wcast-qual"
+warn_flags+=" -xc"
+warn_flags+=" -std=c99" 
+
+
+# Make once with the default compiler, llvm/clang on MacOS, and all
+# the warning flags set
+make --silent "CMD_LINE=$warn_flags" 2>&1 | grep -v 'ar: creating'
+
+
+# This is the big test fan out that takes some time to run. All
+# combinations of configuration flags are compiled and the tests run.
+# All warnings are turned on.  GCC is used since it is more strict.
 while read opts; do
    echo "$opts"
    make clean > /dev/null
-   # Throw away stdout, but not stderr because that's were compiler warnings and erros show
-   # All the extra error check options here so they are run before releases
-   make "CMD_LINE=$opts -Wall -pedantic-errors -Wextra -Wshadow -Wparentheses -Wconversion -xc -std=c99" 2>&1 >/dev/null | grep -v 'ar: creating'
+   # Throw away stdout, but not stderr so the compiler warnings show
+   make --silent "CC=/usr/local/bin/gcc-10" "CMD_LINE=$opts $warn_flags" 2>&1 | grep -v 'ar: creating'
    ./qcbortest > /tmp/bb.$$
    grep SUMMARY /tmp/bb.$$
 done < /tmp/b.$$
